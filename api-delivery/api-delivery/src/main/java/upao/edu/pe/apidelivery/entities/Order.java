71dd518d73d636689fe958c7db1a1c7816e69fcd
Mbgg08 <mgutierrezg11@upao.edu.pe>
2023-04-23 20:46:56 -0500

    feat: Crear clase Order con los atributos id, customerName, customerEmail, status, creationtime, estimatedDeliveryTime, items.

--- api-delivery/api-delivery/src/main/java/upao/edu/pe/apidelivery/entities/Order.java
+++ api-delivery/api-delivery/src/main/java/upao/edu/pe/apidelivery/entities/Order.java
@@ -8,4 +8,77 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
+    private static int id;
+    private String customerName;
+    private String customerEmail;
+    private String status;
+    private LocalDateTime createdTime;
+    private Duration estimatedDeliveryTime;
+    private List<Food> items;
+
+    public Order(String customerName, String customerEmail){
+        this.id = id++;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.status = "en progreso";
+        this.createdTime = LocalDateTime.now();
+        this.estimatedDeliveryTime = Duration.ofMinutes(30);
+        this.items = new ArrayList<>();
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        Order.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(LocalDateTime createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public Duration getEstimatedDeliveryTime() {
+        return estimatedDeliveryTime;
+    }
+
+    public void setEstimatedDeliveryTime(Duration estimatedDeliveryTime) {
+        this.estimatedDeliveryTime = estimatedDeliveryTime;
+    }
+
+    public List<Food> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Food> items) {
+        this.items = items;
+    }
 }
