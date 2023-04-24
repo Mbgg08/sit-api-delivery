67c75dcf471c6806ba0e1f2654e27fe4373a3551
Mbgg08 <mgutierrezg11@upao.edu.pe>
2023-04-23 20:39:30 -0500

    Estructura de Api Delivery

---
+++ api-delivery/api-delivery/src/main/java/upao/edu/pe/apidelivery/entities/Order.java
@@ -0,0 +1,11 @@
+package upao.edu.pe.apidelivery.entities;
+
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+}
